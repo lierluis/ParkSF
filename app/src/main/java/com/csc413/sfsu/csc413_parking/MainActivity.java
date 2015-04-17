dcff886660a146f35d58533dbd8b5440f4e81482
Devin Clary <devinclary@me.com>
2015-04-16 18:20:40 -0700

    Adds an integer variable and comment  to the MainActivity.java file to test branching features with Github

--- app/src/main/java/com/csc413/sfsu/csc413_parking/MainActivity.java
+++ app/src/main/java/com/csc413/sfsu/csc413_parking/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int testForGithub=0; //testing Devin's branch by making a small change for a new commit
     }
 
 
