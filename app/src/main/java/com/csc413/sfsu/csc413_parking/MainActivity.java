26e105e156f51490d62081d2b0253fe5eca00f74
Devin Clary <devinclary@me.com>
2015-04-22 11:15:47 -0700

    Adds support for interacting with a Google Map imbedded in an Activity Fragment.

--- app/src/main/java/com/csc413/sfsu/csc413_parking/MainActivity.java
+++ app/src/main/java/com/csc413/sfsu/csc413_parking/MainActivity.java
@@ -1,19 +1,36 @@
 package com.csc413.sfsu.csc413_parking;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends ActionBarActivity implements OnMapReadyCallback {
+
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
