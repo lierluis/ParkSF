d84bdad41cde7ab889e430bc632c2e55355810d2
Devin Clary <devinclary@me.com>
2015-04-23 18:49:59 -0700

    Manually replaces MainActivity.java and activity_main.xml with the versions from the jeremy_erickson branch to prepare for a merge

--- app/src/main/java/com/csc413/sfsu/csc413_parking/MainActivity.java
+++ app/src/main/java/com/csc413/sfsu/csc413_parking/MainActivity.java
@@ -1,35 +1,71 @@
 package com.csc413.sfsu.csc413_parking;
 
-import android.os.Bundle;
+import com.csc413.sfsu.sfpark_simplified.*;
+
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.*;
+import android.widget.Toast;
+import com.google.android.gms.maps.model.*;
 
 
 public class MainActivity extends ActionBarActivity implements OnMapReadyCallback {
-
     private GoogleMap map;
+    private SFParkQuery query;
+    private SFParkXMLResponse response;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MapFragment mapFragment = (MapFragment) getFragmentManager()
-                .findFragmentById(R.id.map);
+
+        MapFragment mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        query = new SFParkQuery();
+        query.setLatitude(37.792275);
+        query.setLongitude(-122.397089);
+        query.setRadius(0.5);
+        query.setUnitOfMeasurement("MILE");
+        response = new SFParkXMLResponse();
+
     }
 
     @Override
-    public void onMapReady(GoogleMap map) {
-        map.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
-                .title("Marker"));
+    public void onMapReady (GoogleMap map) {
+        this.map = map;
+        LatLng sanFrancisco = new LatLng(37.7750, -122.4183);
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sanFrancisco, 14));
+        //map.setOnMapClickListener(new MapListener(map));
+        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                //String msg = "Latitude: " + latLng.latitude + "\nLongitude: " + latLng.longitude;
+                query.setLongitude(latLng.longitude);
+                query.setLatitude(latLng.latitude);
+                String msg;
+                if (response.populate(query)) {
+                    msg = "Status: " + response.status();
+                    msg += "\nMessage: " + response.message();
+                    if (response.numRecords() > 0) {
+                        for (int i = 0; i < response.avl(0).pts(); i++) {
+                            msg += "\nLocation " + (i+1) + ": ("
+                                    + response.avl(0).loc().longitude(i)
+                                    + ", "
+                                    + response.avl(0).loc().latitude(i)
+                                    + ")";
+                        }
+                    }
+                }
+                else
+                    msg = "failed to populate: " + response.status();
+
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
